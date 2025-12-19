8d534c3a2094c8600c5c43b991d1eef386f69d30
Gabriel Salomon <gabrielsalomon500@gmail.com>
2025-12-18 19:55:42 -0600

    Commit Model

---
+++ app/models/Pelicula.php
@@ -0,0 +1,55 @@
+<?php
+require_once "config/database.php";
+
+class Pelicula {
+
+    public static function listar($busqueda = null) {
+        $db = Database::connect();
+
+        if ($busqueda) {
+            $stmt = $db->prepare(
+                "SELECT * FROM peliculas WHERE titulo LIKE ?"
+            );
+            $stmt->execute(["%$busqueda%"]);
+            return $stmt->fetchAll();
+        }
+
+        return $db->query("SELECT * FROM peliculas")->fetchAll();
+    }
+
+    public static function obtener($id) {
+        $db = Database::connect();
+        $stmt = $db->prepare(
+            "SELECT * FROM peliculas WHERE pelicula_id=?"
+        );
+        $stmt->execute([$id]);
+        return $stmt->fetch();
+    }
+
+    public static function crear($data) {
+        $db = Database::connect();
+        $stmt = $db->prepare(
+            "INSERT INTO peliculas(titulo,director,anio,clasificacion)
+             VALUES (?,?,?,?)"
+        );
+        return $stmt->execute($data);
+    }
+
+    public static function actualizar($data) {
+        $db = Database::connect();
+        $stmt = $db->prepare(
+            "UPDATE peliculas
+             SET titulo=?, director=?, anio=?, clasificacion=?
+             WHERE pelicula_id=?"
+        );
+        return $stmt->execute($data);
+    }
+
+    public static function eliminar($id) {
+        $db = Database::connect();
+        $stmt = $db->prepare(
+            "DELETE FROM peliculas WHERE pelicula_id=?"
+        );
+        return $stmt->execute([$id]);
+    }
+}
