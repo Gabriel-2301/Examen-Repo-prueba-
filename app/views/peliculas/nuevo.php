34c9db67e2709b43616af052c1f7fb9a904612b8
Gabriel Salomon <gabrielsalomon500@gmail.com>
2025-12-18 20:02:57 -0600

    Commit Nuevo

---
+++ app/views/peliculas/nuevo.php
@@ -0,0 +1,9 @@
+<h2>Nueva Película</h2>
+
+<form method="post">
+    <input name="titulo" placeholder="Título"><br>
+    <input name="director" placeholder="Director"><br>
+    <input name="anio" placeholder="Año"><br>
+    <input name="clasificacion" placeholder="Clasificación"><br>
+    <button>Guardar</button>
+</form>
