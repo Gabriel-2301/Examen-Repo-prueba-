99c3a880cef172a16e24acdf8b2ae32f7c9b7ff3
Gabriel Salomon <gabrielsalomon500@gmail.com>
2025-12-18 20:04:22 -0600

    Commit Visualizar

---
+++ app/views/peliculas/visualizar.php
@@ -0,0 +1,8 @@
+<h2>Visualizar Película</h2>
+
+<p><strong>Título:</strong> <?= $pelicula['titulo'] ?></p>
+<p><strong>Director:</strong> <?= $pelicula['director'] ?></p>
+<p><strong>Año:</strong> <?= $pelicula['anio'] ?></p>
+<p><strong>Clasificación:</strong> <?= $pelicula['clasificacion'] ?></p>
+
+<a href="index.php">Volver</a>
