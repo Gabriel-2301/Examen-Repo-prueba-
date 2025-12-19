8cb7818afcd7627be2f1b837c7cc655a905bbae1
Gabriel Salomon <gabrielsalomon500@gmail.com>
2025-12-18 20:03:39 -0600

    Commit Actualizar

---
+++ app/views/peliculas/actualizar.php
@@ -0,0 +1,9 @@
+<h2>Actualizar Película</h2>
+
+<form method="post">
+    <input name="titulo" value="<?= $pelicula['titulo'] ?>"><br>
+    <input name="director" value="<?= $pelicula['director'] ?>"><br>
+    <input name="anio" value="<?= $pelicula['anio'] ?>"><br>
+    <input name="clasificacion" value="<?= $pelicula['clasificacion'] ?>"><br>
+    <button>Actualizar</button>
+</form>
